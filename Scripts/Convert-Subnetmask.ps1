32418faf2b99ec963ff1b283e82bc22aa1e2fec7
BornToBeRoot <BornToBeRoot@users.noreply.github.com>
2016-06-23 15:40:57 +0200

    Update Convert-Subnetmask.ps1

--- Scripts/Convert-Subnetmask.ps1
+++ Scripts/Convert-Subnetmask.ps1
@@ -8,18 +8,15 @@
 
 <#
     .SYNOPSIS
-    
     Convert a subnetmask (XXX.XXX.XXX.XXX) to CIDR (/XX) or vise versa
 
     .DESCRIPTION
-    
     Convert a subnetmask (XXX.XXX.XXX.XXX) to CIDR (/XX) or vise versa
         
     If you found a bug or have some ideas to improve this script... Let me know. You find my Github profile in 
     the links below.
                 
     .EXAMPLE
-    
     .\Convert-Subnetmask.ps1 -CIDR 24
 
     Mask          CIDR
@@ -27,7 +24,6 @@
     255.255.255.0   24
 
     .EXAMPLE
-  
     .\Convert-Subnetmask.ps1 -Mask 255.255.0.0
 
     Mask        CIDR
