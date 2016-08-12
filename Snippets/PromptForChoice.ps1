5ba9683b2bbd571c32f344ec2fe53f87e1a364a0
BornToBeRoot <BornToBeRoot@users.noreply.github.com>
2016-08-12 10:15:47 +0200

    Update PromptForChoice.ps1

--- Snippets/PromptForChoice.ps1
+++ Snippets/PromptForChoice.ps1
@@ -2,8 +2,8 @@ $Title = "Continue"
 $Info = "Would you like to continue?"
  
 $Options = [System.Management.Automation.Host.ChoiceDescription[]] @("&Yes", "&No")
-[int]$defaultchoice = 0
-$Opt =  $host.UI.PromptForChoice($Title , $Info , $Options,$defaultchoice)
+[int]$Defaultchoice = 0
+$Opt =  $host.UI.PromptForChoice($Title , $Info, $Options, $Defaultchoice)
 
 switch($Opt)
 {
